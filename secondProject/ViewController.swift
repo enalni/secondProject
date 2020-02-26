b10e6d08e4cb88bfc9eb4a34c1c951dbeb43bfbb
aa <aaa>
2020-02-26 11:22:47 +0300

    commit

--- secondProject/ViewController.swift
+++ secondProject/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var roundTwo: UIView!
+    @IBOutlet weak var roundOne: UIView!
+    @IBOutlet weak var roundThree: UIView!
+    @IBOutlet var rounds: [UIView]!
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        roundOne.layer.cornerRadius = 50
+        roundTwo.layer.cornerRadius = 50
+        roundThree.layer.cornerRadius = 50
+        button.layer.cornerRadius = 10
+        button.setTitle("NEXT", for: .normal)
     }
+    
 
-
+    @IBAction func nextColor(_ sender: UIView) {
+        
+        
+        for round in rounds {
+            round.alpha = 0.3
+    }
 }
-
+}
